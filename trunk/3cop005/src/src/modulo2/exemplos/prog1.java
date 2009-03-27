d622160780c412ed7ac751a3ec924cda6d5e2e0d
mhadaniya <mhadaniya@263aa8e6-1b01-11de-8120-8d224146dbbc>
2009-03-27 19:14:14 +0000

    1st

---
+++ trunk/3cop005/src/src/modulo2/exemplos/prog1.java
@@ -0,0 +1,24 @@
+package src.modulo2.exemplos;
+
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+
+public class prog1 implements GLEventListener{
+
+    public void init(GLAutoDrawable drawable) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void display(GLAutoDrawable drawable) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void displayChanged(GLAutoDrawable drawable, boolean modeChanged, boolean deviceChanged) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
